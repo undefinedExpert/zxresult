be344ee8db37218d6146ef6f3e849113b2200c00
Emanuel Slotwinski <emcioxd@gmail.com>
2016-06-23 15:05:24 +0200

    preparing to swap expect to chai

--- app/components/SearchForm/tests/index.test.js
+++ app/components/SearchForm/tests/index.test.js
@@ -4,6 +4,40 @@ import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
 
-describe('<SearchForm />', () => {
+describe( '<SearchForm />', () => {
+  let component;
+  beforeEach( () => {
+    component = shallow( <SearchForm /> );
 
-});
+    console.log( component.find( '#feeling' ).length );
+    console.log( '----------------------------------' );
+
+    component.find( '#feeling' ).simulate( 'change', 'sad' );
+  } );
+
+  it( 'Shows text in select input', () => {
+    expect( component.find( '#feeling' ) ).toExist();
+  } );
+
+} );
+//
+// it('Logotype element exist', () => {
+//   const renderedComponent = shallow(<Navigation />);
+//   expect(renderedComponent.find('#logotype').length).toEqual(1);
+// });
+
+// describe('SearchBar', () => {
+//   let component;
+//
+//   describe('Entering search text', () => {
+//     beforeEach(() => {
+//       component = renderComponent(SearchBar);
+//       component.find('.search-bar').simulate('change', 'search value');
+//     });
+//
+//     it('Shows text in search input', () => {
+//       expect(component.find('.search-bar')).to.have.value('search value');
+//     });
+//   });
+//
+// });
