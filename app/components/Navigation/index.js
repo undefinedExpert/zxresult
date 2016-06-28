be40a9f5a186b5a53ced8eb39bfe873498e4d254
Emanuel Slotwinski <emcioxd@gmail.com>
2016-06-28 19:57:42 +0200

    working multiple styles

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -9,15 +9,16 @@ import styles from './styles.css';
 import classNames from 'classnames';
 
 function Navigation() {
+  console.log(styles);
   return (
     <div className={styles.navigation}>
       <div className={styles.bar}>
-        <div className={classNames('bar__item', 'bar__item--left')}>
+        <div className={classNames(styles.bar__item, styles['bar__item--left'])}>
           <div id="logotype" className={styles.logotype}>
             <h3>Logotyp</h3>
           </div>
         </div>
-        <div className={classNames('bar__item', 'bar__item--right')}>
+        <div className={classNames(styles.bar__item, styles['bar__item--right'])}>
           <ul className={styles.links}>
             <li id="loginButton"><a href="#">Log In</a></li>
             <li id="registerButton"><a href="#">Register</a></li>
