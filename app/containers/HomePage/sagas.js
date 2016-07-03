aaa7878bb9ab121fe7b9cdf9bc0e5969f3ad4aab
Emanuel Slotwinski <emcioxd@gmail.com>
2016-07-03 12:14:16 +0200

    update sagas with appropriate genre parameter

--- app/containers/HomePage/sagas.js
+++ app/containers/HomePage/sagas.js
@@ -8,14 +8,16 @@ import _ from 'lodash';
 export function* getRepos() {
   // Select username from store
   const genre = yield select(selectGenre());
-  const genreCapitalized = genre;
+  const genreUpperLetter = _.upperFirst(genre);
   const genreList = yield select(selectGenreList());
-  const genreId = _.findIndex(genreList, ['name', genreCapitalized]);
+  const genreId = _.findIndex(genreList, ['name', genreUpperLetter]);
 
+  debugger;
   console.log(genreId);
   //
-  // const Url = 'http://api.themoviedb.org/3/discover/movie?api_key=9dee05d48efe51f51b15cc63b1fee3f5';
-  // const requestURL = `${Url}&with_genres=${genreId}`;
+  const Url = 'http://api.themoviedb.org/3/discover/movie?api_key=9dee05d48efe51f51b15cc63b1fee3f5';
+  const requestUrl = `${Url}&with_genres=${genreList[genreId].id}`;
+  console.log(requestUrl);
   // // http://api.themoviedb.org/3/discover/movie?api_key=9dee05d48efe51f51b15cc63b1fee3f5&with_keywords=11828&with_genres=16
   //
   // const repos = yield call(request, requestURL);
