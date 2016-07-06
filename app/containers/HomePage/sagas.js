f27012607b24eab600ac6b2c40b89eb696d71fbc
Emanuel Slotwinski <ask@eslotwinski.com>
2016-07-06 23:06:13 +0200

    refactoring sagas

--- app/containers/HomePage/sagas.js
+++ app/containers/HomePage/sagas.js
@@ -10,37 +10,37 @@ import retext from 'retext';
 import nlcstToString from 'nlcst-to-string';
 import keywords from 'retext-keywords';
 
-
-//
-// // var retext = require('retext');
-// // var nlcstToString = require('nlcst-to-string');
-// // var keywords = require('retext-keywords');
-//
-// console.log(keywords);
 // Individual exports for testing
+function randomizePage() {
+  const chance = new Chance();
+  return chance.integer({ min: 0, max: 250 });
+}
+
+function rateKeywords() {
+  //...
+}
+
+function* constructUrl(keywords) {
+  const randomNumber = randomizePage();
+  const filters = yield select(selectFilters());
+  const genreUpperLetter = _.upperFirst(filters.genre);
+  const genreList = filters.genreList;
+  const genreId = _.findIndex(genreList, ['name', genreUpperLetter]);
+  const Url = 'http://api.themoviedb.org/3/discover/movie?api_key=9dee05d48efe51f51b15cc63b1fee3f5';
+
+  return `${Url}&with_genres=${genreList[genreId].id}&page=${randomNumber}`;
+}
+
 export function* getRepos() {
   // Select username from store
-  const chance = new Chance();
-  const filters = yield select(selectFilters());
-
-  const genreUpperLetter = _.upperFirst(filters.genre);
-  const genreList = yield filters.genreList;
-  const genreId = _.findIndex(genreList, ['name', genreUpperLetter]);
-
-  const randomNumber = chance.integer({ min: 0, max: 250 });
-
-
-  //
-  const Url = 'http://api.themoviedb.org/3/discover/movie?api_key=9dee05d48efe51f51b15cc63b1fee3f5';
-  // console.log(genreId);
-  const requestUrl = `${Url}&with_genres=${genreList[genreId].id}&page=${randomNumber}`;
+  const requestUrl = yield constructUrl();
   const movies = yield call(request, requestUrl);
-  // const movies = {};
 
   if (!movies.err) {
     yield put(resultSet(movies.data, movies.data.results[0]));
   }
 }
+
 
 /**
  * Watches for LOAD_REPOS action and calls handler
@@ -54,16 +54,17 @@ export function* getReposWatcher() {
 /**
  * Root saga manages watcher lifecycle
  */
-// export function* githubData() {
-//   // Fork watcher so we can continue execution
-//   const watcher = yield fork(getReposWatcher);
-//
-//   // Suspend execution until location changes
-//   yield take(LOCATION_CHANGE);
-//   yield cancel(watcher);
-// }
+export function* githubData() {
+  // Fork watcher so we can continue execution
+  const watcher = yield fork(getReposWatcher);
+
+  // Suspend execution until location changes
+  yield take(LOCATION_CHANGE);
+  yield cancel(watcher);
+}
 
 // Bootstrap sagas
 export default [
   getReposWatcher,
+  githubData,
 ];
