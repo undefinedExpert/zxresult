56f34a8fe3ad53095adec4507ca4b20a4d76e950
lanskey <emcioxd@gmail.com>
2016-07-14 14:22:47 +0200

    fix eslin problems

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -27,7 +27,7 @@ export default class App extends React.Component { // eslint-disable-line react/
   render() {
     return (
       <div className={styles.app}>
-        <Navigation logotype={true} />
+        <Navigation logotype />
         {this.props.children}
         <BottomNavigation />
       </div>
