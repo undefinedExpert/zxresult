4cc9bfa95403b5a8fe321494fe2da10ca0f0c51f
lanskey <emcioxd@gmail.com>
2016-08-16 16:18:02 +0200

    separate css variables into genres

--- app/css-config.js
+++ app/css-config.js
@@ -3,9 +3,27 @@
  * WHEN YOU RUN THIS FILE RERUN WEBPACK (NPM START)
  * */
 
-module.exports = {
+const _ = require('lodash');
+
+// Css variables for colors
+const colors = {
   cMain: '#2E3440',
-  cSup: '#844350',
+  cMainHover: 'color(var(--cMain) a(45%))',
+  cMainFocus: 'color(var(--cMain) a(45%))',
+  cSup: '#844350', // supplementary color
+  cSupHover: 'color(var(--cSup) a(45%));',
+  cSupFocus: 'color(var(--cSup) a(45%));',
   cHeading: '#C0C5CE',
   cText: '#7B818B',
 };
+
+// Css variables for fonts
+const fonts = {
+  fsText: '1.6rem',            // Font-size Text (1.6rem === 16px)
+};
+
+const variables = _.merge(
+  colors,
+  fonts
+);
+module.exports = variables;
