57720f95b3f13a577b8b924e12d66c215ee17834
lanskey <emcioxd@gmail.com>
2016-09-05 02:30:28 +0200

    Refactor SelectList component

--- app/components/SelectList/tests/index.test.js
+++ app/components/SelectList/tests/index.test.js
@@ -1,12 +1,31 @@
-// import SelectList from '../index';
-//
-// import { expect } from 'chai';
-// import { shallow, render, mount } from 'enzyme';
-// import React from 'react';
+import SelectList from '../index';
+import sinon from 'sinon';
+
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
 
 describe('<SelectList />', () => {
+  let renderComponent;
+  const props = {
+    items: [
+      { value: 'test1', list: [], options: { onChangeHandler: sinon.spy(), title: 'Test1' } },
+      { value: 'test2', list: [], options: { onChangeHandler: () => {}, title: 'Test2' } },
+      { value: 'test3', list: [], options: { onChangeHandler: () => {}, title: 'Test3' } },
+    ],
+  };
+  beforeEach(() => {
+    renderComponent = shallow(<SelectList items={props.items} />, {});
+  });
 
-  // it should render at least 3 items
-  // it should run the onChange callback while the select item change
+  it('Should render at least 3 items', () => {
+    const expected = renderComponent.find('Select');
+    expect(expected.length).to.be.eql(props.items.length);
+  });
 
+  it('Should run onChangeHandler while the select change', () => {
+    const select = renderComponent.find('Select[title="Test1"]');
+    select.simulate('change');
+    expect(select.prop('onChange').calledOnce).to.eql(true);
+  });
 });
