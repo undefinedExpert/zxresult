57f7c0287c530a4b3c44ecb8c1e35a03b96b6559
lanskey <emcioxd@gmail.com>
2016-09-16 15:53:26 +0200

    Refactor

--- app/components/Genres/index.js
+++ app/components/Genres/index.js
@@ -7,23 +7,30 @@
 import React from 'react';
 import GenreIcons from 'components/GenreIcons';
 import styles from './styles.css';
+import Section from 'components/Section';
 
-function Genres() {
+function Genres(props) {
+  const {
+    title,
+    size,
+  } = props;
   return (
-    <ul className={styles.genres}>
-      <li className={styles.item}>
-        <GenreIcons type="Horror" />
-        <h6 className={styles.label}>Action</h6>
-      </li>
-      <li className={styles.item}>
-        <GenreIcons type="SciFi" />
-        <h6 className={styles.label}>Sci-fi</h6>
-      </li>
-      <li className={styles.item}>
-        <GenreIcons type="Comedy" />
-        <h6 className={styles.label}>Comedy</h6>
-      </li>
-    </ul>
+    <Section title={title} size={size}>
+      <ul className={styles.genres}>
+        <li className={styles.item}>
+          <GenreIcons type="Horror" />
+          <h6 className={styles.label}>Action</h6>
+        </li>
+        <li className={styles.item}>
+          <GenreIcons type="SciFi" />
+          <h6 className={styles.label}>Sci-fi</h6>
+        </li>
+        <li className={styles.item}>
+          <GenreIcons type="Comedy" />
+          <h6 className={styles.label}>Comedy</h6>
+        </li>
+      </ul>
+    </Section>
   );
 }
 
