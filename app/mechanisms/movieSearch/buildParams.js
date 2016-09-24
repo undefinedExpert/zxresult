e3c96f11640e710fe253121159cd7d1abec21010
lanskey <emcioxd@gmail.com>
2016-09-24 12:56:20 +0200

    Refactor

--- app/mechanisms/movieSearch/buildParams.js
+++ app/mechanisms/movieSearch/buildParams.js
@@ -1,5 +1,7 @@
 import Chance from 'chance';
+import { each } from 'lodash';
 import { apiUrl, apiKey } from 'containers/App/constants';
+import { mapApiMovieSearchParams } from './constants';
 
 // Add each param to url
 function attachParams(params, baseUrl) {
@@ -20,32 +22,49 @@ export function buildUrlParams(params, endpoint) {
 
 // Randomize page depending on max resultRange
 export function randomizePage(storeParams) {
-  const maxPage = storeParams.range.pages ? storeParams.range.pages > 1000 ? 1000 : storeParams.range.pages : 1;
+  const maxRange = storeParams.range.pages > 1000 ? 1000 : storeParams.range.pages;
+  const maxPage = storeParams.range.pages ? maxRange : 1;
+
   const chance = new Chance();
   // FIXME: There is an issue with defining a default value, when the user first time come to website.
   return chance.integer({ min: 1, max: maxPage });
 }
 
 // TODO: Refactor
+function renameParamsToApiKeys(params) {
+  let paramsContainer = {};
+  each(params, (value, key) => {
+    let queryName = key;
+    queryName = mapApiMovieSearchParams[queryName] || queryName;
+    paramsContainer[queryName] = value;
+  });
+  return paramsContainer;
+}
+
 function prepareParams(storeParams) {
+  console.log('store params: ', storeParams);
   const randomPage = storeParams.range.pages ? randomizePage(storeParams) : null;
   const { genre, decade, trend } = storeParams;
   // Define possible query and check if appropriate option exist, so we could use their options
   const prepared = {
-    with_genres: genre.active ? genre.active.id : null,
+    genre: genre.active ? genre.active.id : null,
     page: randomPage,
-    'primary_release_date.gte': decade.active ? decade.active.rangeMin : null,
-    'primary_release_date.lte': decade.active ? decade.active.rangeMax : null,
-    'vote_count.gte': trend.active ? trend.active.voteRange.min : null,
-    'vote_count.lte': trend.active ? trend.active.voteRange.max : null,
-    'vote_average.gte': trend.active ? trend.active.voteAverage.min : null,
-    'vote_average.lte': trend.active ? trend.active.voteAverage.max : null,
+    rangeMin: decade.active ? decade.active.rangeMin : null,
+    rangeMax: decade.active ? decade.active.rangeMax : null,
+    voteRangeMin: trend.active ? trend.active.voteRange.min : null,
+    voteRangeMax: trend.active ? trend.active.voteRange.max : null,
+    voteAverageMin: trend.active ? trend.active.voteAverage.min : null,
+    voteAverageMax: trend.active ? trend.active.voteAverage.max : null,
   };
+
+  const renamedParams = renameParamsToApiKeys(prepared);
+  console.log(renamedParams);
 
   // Remove null keys so they won't be used in our url
   Object.keys(prepared).forEach((key) => {
     if (!prepared[key]) delete prepared[key];
   });
+
   return prepared;
 }
 
---
+++ app/mechanisms/movieSearch/constants.js
@@ -0,0 +1,11 @@
+export const mapApiMovieSearchParams = {
+  genre: 'with_genres',
+  page: 'page',
+  rangeMin: 'primary_release_date.gte',
+  rangeMax: 'primary_release_date.lte',
+  voteRangeMin: 'vote_count.gte',
+  voteRangeMax: 'vote_count.lte',
+  voteAverageMin: 'vote_average.gte',
+  voteAverageMax: 'vote_average.lte',
+};
+
