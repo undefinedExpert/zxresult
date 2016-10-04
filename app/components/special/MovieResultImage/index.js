04b6f0222e3a9ac918640d80896a1a3bc20a2c1a
lanskey <emcioxd@gmail.com>
2016-10-04 20:22:46 +0200

    Prepare dragging image

--- app/components/special/MovieResultImage/index.js
+++ app/components/special/MovieResultImage/index.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './styles.css';
+import _ from 'lodash';
 
 class MovieResultImage extends React.Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class MovieResultImage extends React.Component {
     this.state = { isAnimationBegin: true, top: 0, bottom: 0, imageHeight: 0 };
   }
 
-  imageLoad = (e) => {
+  onLoadHandler = (e) => {
     // TODO: Create scan animation
     const path = e.target.attributes.src.value;
     const pathBigger = path.replace(/\w45/g, 'w154');
@@ -27,6 +28,16 @@ class MovieResultImage extends React.Component {
     // console.log(e.target.offsetHeight);
   };
 
+  onDragEnterHandler = (e) => {
+    // var crt = this.cloneNode(true);
+    // crt.style.backgroundColor = "red";
+    // e.dataTransfer.setDragImage(crt, 0, 0);
+    e.preventDefault()
+    var crt = e.target.cloneNode(true);
+    crt.style.display = "none";
+
+  }
+
   render() {
     const {
       path,
@@ -34,10 +45,13 @@ class MovieResultImage extends React.Component {
     } = this.props;
     return (
       <div className={styles.resultImage}>
-        <div className={styles.overlay}></div>
-        <div className={styles.imageContainer}>
+        {/*<div className={styles.overlay}></div>*/}
+        <div draggable="false" className={styles.imageContainer}>
           <img
-            onLoad={this.imageLoad}
+            draggable="true"
+            onDragEnter={this.onDragEnterHandler}
+            ref="photo"
+            onLoad={this.onLoadHandler}
             alt={alt}
             className={styles.image}
             src={`http://image.tmdb.org/t/p/w45/${path}`}
