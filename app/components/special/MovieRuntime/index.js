aa8797869ebbf97b2441777125a3f931a4631210
lanskey <emcioxd@gmail.com>
2016-10-10 23:17:07 +0200

    Quick MovieRuntime Component refactor

--- app/components/special/MovieRuntime/index.js
+++ app/components/special/MovieRuntime/index.js
@@ -1,27 +1,36 @@
 /**
-*
-* MovieRuntime
-*
-*/
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
+ */
 
-import React from 'react';
-import styles from './styles.css';
+import React, { PropTypes as ptype } from 'react';
 import { IoClock } from 'react-icons/lib/io/';
+
 import Section from 'components/general/Section';
 
-function MovieRuntime(props) {
-  const {
-    sectionSize = '1/2',
-  } = props;
+import styles from './styles.css';
+
+
+/**
+ * MovieRuntime
+ * @desc Renders length of the movie with an icon
+ * returns packed runtime section
+ * TODO: Make this working on real data.
+ */
+function MovieRuntime({ sectionSize = '1/2' }) {
+  const title = 'Runtime';
+
   return (
-    <Section title={'Runtime'} size={sectionSize}>
+    <Section title={title} size={sectionSize}>
       <span><IoClock className={styles.icon} size={50} /> 2h 31min</span>
     </Section>
   );
 }
 
 MovieRuntime.propTypes = {
-  sectionSize: React.PropTypes.string,
+  sectionSize: ptype.string,
 };
 
 export default MovieRuntime;
