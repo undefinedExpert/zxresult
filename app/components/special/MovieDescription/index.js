7d20f7ba972306a0b7de1af541bdfea10d74dfbc
lanskey <emcioxd@gmail.com>
2016-10-10 23:18:32 +0200

    Quick MovieDescription Component refactor

--- app/components/special/MovieDescription/index.js
+++ app/components/special/MovieDescription/index.js
@@ -1,34 +1,45 @@
 /**
-*
-* MovieDescription
-*
-*/
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
+ */
 
-import React from 'react';
+import React, { PropTypes as ptype } from 'react';
 import { truncate } from 'lodash';
-import styles from './styles.css';
+
 import Section from 'components/general/Section';
 
+import styles from './styles.css';
+
+
+/**
+ * MovieDescription
+ * @desc Check if movie description exist, truncate it if yes, or display appropriate errMsh
+ * TODO: Ability to show full description (when user clicks on it)
+ */
 function MovieDescription(props) {
   const {
     description,
     limit,
-    sectionSize = '1/1',
-  } = props;
+    sectionSize = '1/1' } = props;
+
   const title = 'Description';
   const errMsg = 'Description isn\'t available';
   const errMsgReturnedByApi = 'No overview found.';
+
+  const cs = styles.description;
   return (
-    <Section size={sectionSize} title={title} className={styles.description}>
+    <Section size={sectionSize} title={title} className={cs}>
       <p>{description !== errMsgReturnedByApi ? truncate(description, { length: limit }) : errMsg}</p>
     </Section>
   );
 }
 
 MovieDescription.propTypes = {
-  description: React.PropTypes.string,
-  limit: React.PropTypes.number,
-  sectionSize: React.PropTypes.string,
+  description: ptype.string,
+  limit: ptype.number,
+  sectionSize: ptype.string,
 };
 
 export default MovieDescription;
