0073852d847bf9169eda2b0e35dfec62e0d89246
lanskey <emcioxd@gmail.com>
2016-10-10 23:17:20 +0200

    Quick MovieGallery Component refactor

--- app/components/special/MovieGallery/index.js
+++ app/components/special/MovieGallery/index.js
@@ -1,34 +1,46 @@
 /**
-*
-* Gallery
-*
-*/
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
+ */
 
-import React from 'react';
-import styles from './styles.css';
+import React, { PropTypes as ptype } from 'react';
+
 import Section from 'components/general/Section';
 import ResultImage from 'components/special/MovieResultImage';
 import MovieSearchForm from 'containers/MovieSearchForm';
 
+import styles from './styles.css';
 
+
+/**
+ * MovieGallery
+ * @desc Render Gallery and filters.
+ * TODO: Rename MovieGallery
+ * TODO: Working Gallery, not just single image.
+ */
 function MovieGallery(props) {
   const {
     path,
     alt,
-    orientation = 'horizontal',
-  } = props;
+    orientation = 'horizontal' } = props;
+
+  const altMsg = `${alt} poster`;
+
+  const cs = styles.gallery;
   return (
-    <Section className={styles.gallery}>
-      <ResultImage path={path} alt={`${alt} poster`} />
+    <Section className={cs}>
+      <ResultImage path={path} alt={altMsg} />
       <MovieSearchForm orientation={orientation} />
     </Section>
   );
 }
 
 MovieGallery.propTypes = {
-  path: React.PropTypes.string,
-  alt: React.PropTypes.string,
-  orientation: React.PropTypes.string,
+  path: ptype.string,
+  alt: ptype.string,
+  orientation: ptype.string,
 };
 
 export default MovieGallery;
