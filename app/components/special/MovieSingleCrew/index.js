1cc59d48feee8df5a5ecea791537d8cf2b9ac982
lanskey <emcioxd@gmail.com>
2016-10-10 23:18:09 +0200

    Quick MovieSingleCrew Component refactor

--- app/components/special/MovieSingleCrew/index.js
+++ app/components/special/MovieSingleCrew/index.js
@@ -1,22 +1,28 @@
 /**
-*
-* SingleCrew
-*
-*/
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
+ */
 
-import React from 'react';
+import React, { PropTypes as ptype } from 'react';
+
 import styles from './styles.css';
 
-function MovieSingleCrew(props) {
-  const {
-    path,
-    alt,
-  } = props;
+
+/**
+ * MovieSingleCrew
+ * @desc Single representation of a crew member.
+ * @memberOf MovieCrewList
+ * TODO: Make this working with real data.
+ */
+function MovieSingleCrew({ path, alt }) {
+  const sourcePath = `http://image.tmdb.org/t/p/original/${path}`;
   return (
     <div>
       <div className={styles.image}>
         <img
-          src={`http://image.tmdb.org/t/p/original/${path}`}
+          src={sourcePath}
           alt={alt}
         />
       </div>
@@ -27,8 +33,8 @@ function MovieSingleCrew(props) {
 }
 
 MovieSingleCrew.propTypes = {
-  path: React.PropTypes.string,
-  alt: React.PropTypes.string,
+  path: ptype.string,
+  alt: ptype.string,
 };
 
 export default MovieSingleCrew;
