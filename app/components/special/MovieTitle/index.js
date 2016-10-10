8beea48156da7d3c53a325e944cdad9022f0309a
lanskey <emcioxd@gmail.com>
2016-10-10 23:16:34 +0200

    Quick MovieTitle Component refactor

--- app/components/special/MovieTitle/index.js
+++ app/components/special/MovieTitle/index.js
@@ -1,28 +1,37 @@
 /**
-*
-* MovieTitle
-*
-*/
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
+ */
 
-import React from 'react';
-import styles from './styles.css';
+import React, { PropTypes as ptype } from 'react';
+
 import Section from 'components/general/Section';
 
-function MovieTitle(props) {
-  const {
-    movieTitle,
-    sectionSize = '1/1',
-  } = props;
+import styles from './styles.css';
+
+
+/**
+ * MovieTitle
+ * @desc Renders movie title
+ * @param movieTitle - Movie title provided by data
+ * @param sectionSize - Size of this section.
+ * TODO: Make date working, it has to work on real data.
+ */
+function MovieTitle({ movieTitle, sectionSize = '1/1' }) {
+  const title = 'Title';
+
   return (
-    <Section size={sectionSize} title={'Title'}>
+    <Section size={sectionSize} title={title}>
       <h1 className={styles.title}>{movieTitle} <span className={styles.date}>(2016)</span></h1>
     </Section>
   );
 }
 
 MovieTitle.propTypes = {
-  movieTitle: React.PropTypes.string,
-  sectionSize: React.PropTypes.string,
+  movieTitle: ptype.string,
+  sectionSize: ptype.string,
 };
 
 export default MovieTitle;
