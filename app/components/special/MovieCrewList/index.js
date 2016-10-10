b8a193a809a55bd6df87c043d9277841c5a824f7
lanskey <emcioxd@gmail.com>
2016-10-10 23:17:43 +0200

    Quick MovieCrewList Component refactor

--- app/components/special/MovieCrewList/index.js
+++ app/components/special/MovieCrewList/index.js
@@ -1,41 +1,48 @@
 /**
- *
- * CrewList
- *
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
  */
 
-import React from 'react';
+import React, { PropTypes as ptype } from 'react';
+
 import Section from 'components/general/Section';
 import SingleCrew from 'components/special/MovieSingleCrew';
 
-function MovieCrewList(props) {
+/**
+ * @desc Render Single Crew Component packed with section basing on provided data.
+ */
+const renderSingle = (item, index) => {
   const {
-    items,
-  } = props;
+    image,
+    alt,
+    title,
+    sectionSize } = item;
 
-  function renderSingle(item, index) {
-    const {
-      image,
-      alt,
-      title,
-      sectionSize,
-    } = item;
-    const errMsg = `${title} isn't available`;
-    return (
-      <Section title={title} size={sectionSize} key={index}>
-        {title ? <SingleCrew path={image} alt={alt} /> : <p>{errMsg}</p>}
-      </Section>
-    );
-  }
+  const errMsg = `${title} isn't available`;
+  return (
+    <Section title={title} size={sectionSize} key={index}>
+      {title ? <SingleCrew path={image} alt={alt} /> : <p>{errMsg}</p>}
+    </Section>
+  );
+};
+
+
+/**
+ * MovieCrewList
+ * @desc Check if there is at least one CrewItem, if yes map & render our single Crew mate.
+ */
+function MovieCrewList({ items }) {
   return (
     <div>
-      {items.length ? items.map(renderSingle) : null}
+      {items.length ? items.map((item, index) => renderSingle(item, index)) : null}
     </div>
-  );
+    );
 }
 
 MovieCrewList.propTypes = {
-  items: React.PropTypes.array,
+  items: ptype.array,
 };
 
 export default MovieCrewList;
