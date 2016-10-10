698cae95e086cdce44316803b98cf27a0136866c
lanskey <emcioxd@gmail.com>
2016-10-10 17:57:51 +0200

    Refactor into stateless Component

--- app/components/general/SelectList/index.js
+++ app/components/general/SelectList/index.js
@@ -5,7 +5,7 @@
  *  3. Module
  */
 
-import React, { PropTypes as ptype, Component } from 'react';
+import React, { PropTypes as ptype } from 'react';
 
 import Select from 'components/general/Select';
 
@@ -13,12 +13,44 @@ import styles from './styles.css';
 
 
 /**
- * Default values for our item, we are set default values here in purpose.
- * to avoid unnecessary complex structure in our Class.
+ * @desc Render Single Select Component, based on it's props.
+ * @param item - Data for our select field
+ * @param index - Used as React key
  */
-const defaultValue = { name: '' };
-const defaultOptions = { title: '', onChangeHandler: () => {} };
+const renderSelect = (item, index) => {
+  /**
+   * Default values for our item, we are set default values here in purpose.
+   * to avoid unnecessary complex structure.
+   */
+  const defaultValue = { name: '' };
+  const defaultOptions = { title: '', onChangeHandler: () => {} };
+  const {
+    value = defaultValue,
+    list,
+    options = defaultOptions } = item;
 
+  const cs = styles.selectItem;
+  return (
+    <div key={index} className={cs}>
+      <Select
+        value={value}
+        isLoading={list <= 0}
+        options={list}
+        onChange={options.onChangeHandler}
+        title={options.title}
+      />
+    </div>
+  );
+};
+
+
+/**
+ * @desc Map Single Select
+ * @param items - Set of Select data.
+ */
+function renderSelects(items) {
+  return items.map((item, index) => renderSelect(item, index));
+}
 
 /**
  * SelectList
@@ -30,38 +62,14 @@ const defaultOptions = { title: '', onChangeHandler: () => {} };
  * @method renderSelect - Renders single Select component from data, handles map function.
  * We are also preparing our Select Component with default data, if they are not provided.
  */
-class SelectList extends Component {
-  renderSelect = (item, index) => {
-    const {
-      value = defaultValue,
-      list,
-      options = defaultOptions } = item;
-
-    const cs = styles.selectItem;
-    return (
-      <div key={index} className={cs}>
-        <Select
-          value={value}
-          isLoading={list <= 0}
-          options={list}
-          onChange={options.onChangeHandler}
-          title={options.title}
-        />
-      </div>
-    );
-  };
-
-  render() {
-    const { items } = this.props;
-
-    const cs = styles.selectList;
-    return (
-      <div className={cs}>
-        {items.map(this.renderSelect)}
-      </div>
-    );
-  }
-}
+const SelectList = ({ items }) => {
+  const cs = styles.selectList;
+  return (
+    <div className={cs}>
+      {renderSelects(items)}
+    </div>
+  );
+};
 
 SelectList.propTypes = {
   title: ptype.string,
