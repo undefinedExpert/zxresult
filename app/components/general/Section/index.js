74b28333636c965fc76ec078097ddcd9e25e16fd
lanskey <emcioxd@gmail.com>
2016-10-10 15:41:33 +0200

    Fix typo

--- app/components/general/Section/index.js
+++ app/components/general/Section/index.js
@@ -1,5 +1,5 @@
 /**
- *  Component are imported in specific (scope based) order:
+ *  Components are imported in specific (scope based) order:
  *  1. Node_modules
  *  2. Application
  *  3. Module
