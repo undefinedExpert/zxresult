76a09e95b90bd9767c68c9a097b01e3035d58a52
lanskey <emcioxd@gmail.com>
2016-10-10 13:43:23 +0200

    Removing trashes

--- app/components/special/MovieResultImage/index.js
+++ app/components/special/MovieResultImage/index.js
@@ -3,20 +3,14 @@
  * ResultImage
  *
  */
-
+// TODO: Create animation && slider
 import React from 'react';
-import ReactDOM from 'react-dom';
 import styles from './styles.css';
-import _ from 'lodash';
 
 class MovieResultImage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isAnimationBegin: true, top: 0, bottom: 0, imageHeight: 0 };
-  }
+  state = {};
 
   onLoadHandler = (e) => {
-    // TODO: Create scan animation
     const path = e.target.attributes.src.value;
     const pathBigger = path.replace(/\w45/g, 'w154');
     const pathBiggest = path.replace(/\w154/g, 'w780');
@@ -25,31 +19,15 @@ class MovieResultImage extends React.Component {
     }
     if (path === pathBigger) return;
     e.target.setAttribute('src', pathBigger);
-    // console.log(e.target.offsetHeight);
   };
 
-  onDragEnterHandler = (e) => {
-    // var crt = this.cloneNode(true);
-    // crt.style.backgroundColor = "red";
-    // e.dataTransfer.setDragImage(crt, 0, 0);
-    e.preventDefault()
-    var crt = e.target.cloneNode(true);
-    crt.style.display = "none";
-
-  }
-
   render() {
-    const {
-      path,
-      alt,
-    } = this.props;
+    const { path, alt } = this.props;
     return (
       <div className={styles.resultImage}>
-        {/*<div className={styles.overlay}></div>*/}
-        <div draggable="false" className={styles.imageContainer}>
+        {/* <div className={styles.overlay}></div> */}
+        <div className={styles.imageContainer}>
           <img
-            draggable="true"
-            onDragEnter={this.onDragEnterHandler}
             ref="photo"
             onLoad={this.onLoadHandler}
             alt={alt}
