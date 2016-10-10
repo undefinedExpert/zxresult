9f0329ad89d5a54f23d70326c216b423d2dd142b
lanskey <emcioxd@gmail.com>
2016-10-10 23:17:33 +0200

    Quick MovieArticle Component refactor

--- app/components/special/MovieArticle/index.js
+++ app/components/special/MovieArticle/index.js
@@ -1,11 +1,12 @@
 /**
-*
-* MovieArticle
-*
-*/
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
+ */
 
-import React from 'react';
-import styles from './styles.css';
+import React, { PropTypes as ptype } from 'react';
+
 import Genres from 'components/special/MovieGenres';
 import MovieTitle from 'components/special/MovieTitle';
 import CrewList from 'components/special/MovieCrewList';
@@ -13,10 +14,15 @@ import HeartRate from 'components/special/MovieHeartRate';
 import MovieRuntime from 'components/special/MovieRuntime';
 import MovieDescription from 'components/special/MovieDescription';
 
-function MovieArticle(props) {
-  const {
-    movie,
-  } = props;
+import styles from './styles.css';
+
+
+/**
+ * MovieArticle
+ * @desc Wraps Components which represent any movie information.
+ */
+function MovieArticle({ movie }) {
+  // TODO: Crew list fetch
   const crewItems = [
     { image: movie.backdrop_path, alt: 'test', title: 'Director', sectionSize: '1/3' },
     { image: movie.backdrop_path, alt: 'test', title: 'Cast', sectionSize: '1/3' },
@@ -26,8 +32,10 @@ function MovieArticle(props) {
     voteAverage: movie.vote_average,
     voteCount: movie.vote_average,
   };
+
+  const cs = styles.movieArticle;
   return (
-    <article className={styles.movieArticle}>
+    <article className={cs}>
       <MovieTitle movieTitle={movie.title} />
       <HeartRate votes={votes} />
       <MovieDescription description={movie.overview} limit={160} />
@@ -39,8 +47,8 @@ function MovieArticle(props) {
 }
 
 MovieArticle.propTypes = {
-  movie: React.PropTypes.object,
-  className: React.PropTypes.string,
+  movie: ptype.object,
+  className: ptype.string,
 };
 
 export default MovieArticle;
