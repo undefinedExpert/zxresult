9981809f8e3701f22fafe69aeb4c6a1a23212d17
lanskey <emcioxd@gmail.com>
2016-10-10 23:19:00 +0200

    Quick MovieResultImage Component refactor

--- app/components/special/MovieResultImage/index.js
+++ app/components/special/MovieResultImage/index.js
@@ -1,38 +1,83 @@
 /**
- *
- * ResultImage
- *
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
  */
-// TODO: Create animation && slider
-import React from 'react';
+
+import React, { PropTypes as ptype, Component } from 'react';
+import { curry } from 'lodash';
+
+import { convertToPattern } from 'utils/hooks';
+
 import styles from './styles.css';
 
-class MovieResultImage extends React.Component {
-  state = {};
+
+/**
+ * @desc Default values for image size loaders. loaded is a representation of: Is this image is loaded?
+ * Pattern is a function which replace specific part of URL, so we could download bigger image.
+ */
+const smallDefaultState = { loaded: false, pattern: convertToPattern(/\w45/g, 'w154') };
+const mediumDefaultState = { loaded: false, pattern: convertToPattern(/\w154/g, 'w500') };
+
+
+/**
+ * MovieResultImage
+ * @desc Render single result image, it provides primitive lazy-load solution
+ * returns packed prop.children with title and appropriate grid size.
+ *
+ * @method componentWillReceiveProps - Reset image loaders state to default
+ * @method onLoadHandler - Handles onLoad event, loads bigger images.
+ * @method handleImageSizeLoading - helps with updating state for appropriate image size.
+ *
+ * returns single result image.
+ */
+class MovieResultImage extends Component {
+  state = {
+    small: smallDefaultState,
+    medium: mediumDefaultState,
+  };
+
+  componentWillReceiveProps() {
+    this.setState({ small: smallDefaultState, medium: mediumDefaultState });
+  }
 
   onLoadHandler = (e) => {
+    const {
+      small,
+      medium } = this.state;
+
     const path = e.target.attributes.src.value;
-    const pathBigger = path.replace(/\w45/g, 'w154');
-    const pathBiggest = path.replace(/\w154/g, 'w780');
-    if (path === pathBigger || path === pathBiggest) {
-      e.target.setAttribute('src', pathBiggest);
+    const setAttr = curry((pattern) => e.target.setAttribute('src', pattern));
+    const loadImage = curry((imageSize) => this.handleImageSizeLoading(setAttr, path, imageSize));
+
+    if (!small.loaded) {
+      loadImage('small');
     }
-    if (path === pathBigger) return;
-    e.target.setAttribute('src', pathBigger);
+    else if (small.loaded && !medium.loaded) {
+      loadImage('medium');
+    }
   };
+
+  handleImageSizeLoading(setAttr, path, imageSize) {
+    const which = this.state[imageSize];
+
+    setAttr(which.pattern(path));
+    this.setState({ [imageSize]: { loaded: true } });
+  }
 
   render() {
     const { path, alt } = this.props;
     return (
       <div className={styles.resultImage}>
-        {/* <div className={styles.overlay}></div> */}
+        <div className={styles.overlay}></div>
         <div className={styles.imageContainer}>
           <img
             ref="photo"
             onLoad={this.onLoadHandler}
             alt={alt}
             className={styles.image}
-            src={`http://image.tmdb.org/t/p/w45/${path}`}
+            src={`http://image.tmdb.org/t/p/w45${path}`}
           />
         </div>
       </div>
@@ -41,8 +86,8 @@ class MovieResultImage extends React.Component {
 }
 
 MovieResultImage.propTypes = {
-  path: React.PropTypes.string,
-  alt: React.PropTypes.string,
+  path: ptype.string,
+  alt: ptype.string,
 };
 
 export default MovieResultImage;
