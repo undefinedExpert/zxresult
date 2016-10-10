1876b88ad6e27182015d119299ccdc97836fc278
lanskey <emcioxd@gmail.com>
2016-10-10 23:16:43 +0200

    Quick WelcomeText Component refactor

--- app/components/special/WelcomeText/index.js
+++ app/components/special/WelcomeText/index.js
@@ -1,17 +1,26 @@
 /**
-*
-* WelcomeText
-*
-*/
-
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
+ */
 import React from 'react';
+
 import styles from './styles.css';
 
+
+/**
+ * WelcomeText
+ * @desc Welcomes user with cool intro message.
+ * TODO: Make it more interesting or sexy.
+ */
 function WelcomeText() {
+  const heading = 'Hi, are you looking for a movie?';
+  const subHeading = 'Let me inspire you then!';
   return (
     <div className={styles.welcomeText}>
-      <h1>Hi, are you looking for a movie?</h1>
-      <h3>Let me inspire you then!</h3>
+      <h1>{heading}</h1>
+      <h3>{subHeading}</h3>
     </div>
   );
 }
