8add518aed82bd335daf0a2fb4a1c1118bfeb68d
lanskey <emcioxd@gmail.com>
2016-10-10 17:57:59 +0200

    Refactor

--- app/components/general/Title/index.js
+++ app/components/general/Title/index.js
@@ -1,26 +1,38 @@
 /**
- *
- * Title
- *
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
  */
 
-import React from 'react';
-import styles from './styles.css';
+import React, { PropTypes as ptype } from 'react';
 import classNames from 'classnames';
 
+import styles from './styles.css';
+
+/**
+ * Title
+ * @desc Helps to render title for each Component which request it.
+ * returns packed, ready to use Title Component.
+ */
 function Title(props) {
-  const className = props.theme === 'light' ? classNames(styles.title, styles['--light']) : styles.title;
+  const {
+    theme,
+    text,
+    children } = props;
+
+  const cs = classNames(styles.title, styles[`--${theme}`]);
   return (
-    <h4 className={className}>
-      {props.text ? props.text : props.children}
+    <h4 className={cs}>
+      {text || children}
     </h4>
   );
 }
 
 Title.propTypes = {
-  children: React.PropTypes.node,
-  text: React.PropTypes.string,
-  theme: React.PropTypes.string,
+  children: ptype.node,
+  text: ptype.string,
+  theme: ptype.string,
 };
 
 export default Title;
