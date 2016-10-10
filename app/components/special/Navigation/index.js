876f6aa0fa3b1146ff4a428b0bbb0a03859a3fe2
lanskey <emcioxd@gmail.com>
2016-10-10 23:16:20 +0200

    Quick navigation Component refactor

--- app/components/special/Navigation/index.js
+++ app/components/special/Navigation/index.js
@@ -1,51 +1,72 @@
 /**
- *
- * Navigation
- *
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
  */
 
-import React from 'react';
-import styles from './styles.css';
+import React, { PropTypes as ptype } from 'react';
 import classNames from 'classnames';
 
+import styles from './styles.css';
+
+/**
+ * Navigation
+ * @desc Renders our app navigation.
+ *
+ * @method renderLogotype - Renders logotype (left side)
+ * @method renderLinks - Render links (right side)
+ * @method renderSide - Helper function to render each side of our bar
+ * TODO: Refactor this method so it can work with multiple callbacks, it might be also worth to implement this as separated Component?
+ * @method renderBar - Renders navigation with.
+ *
+ * TODO: Refactor Navigation Component
+ */
 class Navigation extends React.Component {
-  renderLogotype() {
+  renderLogotype = () => (
+    <div id="logotype" className={styles.logotype}>
+      <h3>Logotyp</h3>
+    </div>
+  );
+
+  renderLinks = () => (
+    <ul className={styles.links}>
+      <li id="loginButton"><a href="#">Log In</a></li>
+      <li id="registerButton"><a href="#">Register</a></li>
+    </ul>
+  );
+
+  renderSide = (side, callback) => {
+    const whichSide = `bar__item--${side}`;
+
+    const cs = classNames(styles.bar__item, styles[whichSide]);
     return (
-      <div id="logotype" className={styles.logotype}>
-        <h3>Logotyp</h3>
+      <div className={cs}>
+        {callback()}
       </div>
     );
-  }
+  };
 
-  renderLinks() {
-    return (
-      <ul className={styles.links}>
-        <li id="loginButton"><a href="#">Log In</a></li>
-        <li id="registerButton"><a href="#">Register</a></li>
-      </ul>
-    );
-  }
+  renderBar = () => (
+    <div className={styles.bar}>
+      {this.renderSide('left', this.renderLogotype)}
+      {this.renderSide('right', this.renderLinks)}
+    </div>
+  );
 
   render() {
     return (
       <div className={styles.navigation}>
-        <div className={styles.bar}>
-          <div className={classNames(styles.bar__item, styles['bar__item--left'])}>
-            {this.logotype ? this.renderLogotype() : null}
-          </div>
-          <div className={classNames(styles.bar__item, styles['bar__item--right'])}>
-            {this.renderLinks}
-          </div>
-        </div>
+        {this.renderBar()}
       </div>
     );
   }
 }
 
 Navigation.propTypes = {
-  logotype: React.PropTypes.bool,
-  filters: React.PropTypes.array,
-  links: React.PropTypes.array,
+  logotype: ptype.bool,
+  filters: ptype.array,
+  links: ptype.array,
 };
 
 export default Navigation;
