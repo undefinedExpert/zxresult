f0e18fdd7a932b4c93001ccfeee5128075075e3a
lanskey <emcioxd@gmail.com>
2016-10-11 15:01:49 +0200

    Move filters constants

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -5,26 +5,11 @@
  */
 import { createRequestTypes } from 'utils/hooks';
 
-// DECADE CONSTANTS
-const UPDATE_FILTER_DECADE = createRequestTypes('app/App/UPDATE_FILTER_DECADE');
-const UPDATE_FILTER_DECADE_LIST = createRequestTypes('app/App/UPDATE_FILTER_DECADE_LIST');
-
-// TREND CONSTANTS
-const UPDATE_FILTER_TREND = createRequestTypes('app/App/UPDATE_FILTER_TREND');
-const UPDATE_FILTER_TREND_LIST = createRequestTypes('app/App/UPDATE_FILTER_TREND_LIST');
-
-// GENRE CONSTANTS
-const UPDATE_FILTER_GENRE = createRequestTypes('app/App/UPDATE_FILTER_GENRE');
-const UPDATE_FILTER_GENRE_LIST = createRequestTypes('app/App/UPDATE_FILTER_GENRE_LIST');
-
 // RESULT SET
 const UPDATE_MOVIE_RESULT = createRequestTypes('app/App/UPDATE_MOVIE_RESULT');
 
 // Update url SET
 const UPDATE_URL = createRequestTypes('app/App/UPDATE_URL');
-
-// Update Result filters
-const UPDATE_FILTERS = createRequestTypes('app/App/UPDATE_FILTERS');
 
 // Analyse movie results to pick the best
 const ANALYSE_MOVIE = createRequestTypes('app/App/ANALYSE_MOVIE');
@@ -47,15 +32,8 @@ const apiUrl = 'http://api.themoviedb.org/3';
 const apiKey = 'api_key=9dee05d48efe51f51b15cc63b1fee3f5';
 
 export {
-  UPDATE_FILTER_DECADE,
-  UPDATE_FILTER_DECADE_LIST,
-  UPDATE_FILTER_TREND,
-  UPDATE_FILTER_TREND_LIST,
-  UPDATE_FILTER_GENRE,
-  UPDATE_FILTER_GENRE_LIST,
   UPDATE_MOVIE_RESULT,
   UPDATE_URL,
-  UPDATE_FILTERS,
   ANALYSE_MOVIE,
   QUEUE_MOVIES,
   UPDATE_SINGLE_MOVIE,
--- app/containers/MovieSearchForm/constants.js
+++ app/containers/MovieSearchForm/constants.js
@@ -1,3 +1,31 @@
 /**
  * Created by Lansky on 11.10.2016.
  */
+// TODO: Change ref links in constants
+import { createRequestTypes } from 'utils/hooks';
+
+// DECADE CONSTANTS
+const UPDATE_FILTER_DECADE = createRequestTypes('app/App/UPDATE_FILTER_DECADE');
+const UPDATE_FILTER_DECADE_LIST = createRequestTypes('app/App/UPDATE_FILTER_DECADE_LIST');
+
+// TREND CONSTANTS
+const UPDATE_FILTER_TREND = createRequestTypes('app/App/UPDATE_FILTER_TREND');
+const UPDATE_FILTER_TREND_LIST = createRequestTypes('app/App/UPDATE_FILTER_TREND_LIST');
+
+// GENRE CONSTANTS
+const UPDATE_FILTER_GENRE = createRequestTypes('app/App/UPDATE_FILTER_GENRE');
+const UPDATE_FILTER_GENRE_LIST = createRequestTypes('app/App/UPDATE_FILTER_GENRE_LIST');
+
+// Update Result filters
+const UPDATE_FILTERS = createRequestTypes('app/App/UPDATE_FILTERS');
+
+export {
+  UPDATE_FILTER_DECADE,
+  UPDATE_FILTER_DECADE_LIST,
+  UPDATE_FILTER_TREND,
+  UPDATE_FILTER_TREND_LIST,
+  UPDATE_FILTER_GENRE,
+  UPDATE_FILTER_GENRE_LIST,
+  UPDATE_FILTERS,
+};
+
