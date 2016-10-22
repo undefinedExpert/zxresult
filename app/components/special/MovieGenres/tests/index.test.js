59f47b7ffa103228f0eaaf989d8658b1c7e0a0dd
lanskey <emcioxd@gmail.com>
2016-10-22 14:52:42 +0200

    Fix unit tests

--- app/components/special/MovieGenres/tests/index.test.js
+++ app/components/special/MovieGenres/tests/index.test.js
@@ -1,9 +1,25 @@
-// import Genres from '../index';
-//
-// import { expect } from 'chai';
-// import { shallow, render, mount } from 'enzyme';
-// import React from 'react';
+/**
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
+ */
+
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+
+import Genres from '../index';
+
 
 describe('<Genres />', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = mount(<Genres />, {});
+  });
 
+  it('Should contain 3 GenreIcons components', () => {
+    const expected = renderComponent.find('img');
+    expect(expected).to.have.length(3);
+  });
 });
