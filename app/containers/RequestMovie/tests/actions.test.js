174a612e31206e72d776f434c9b3a840743d09ee
lanskey <emcioxd@gmail.com>
2016-10-28 13:04:26 +0200

    Updates tests

--- app/containers/RequestMovie/tests/actions.test.js
+++ app/containers/RequestMovie/tests/actions.test.js
@@ -7,22 +7,104 @@
 
 import { expect } from 'chai';
 
-import * as CONSTANT from './../constants';
+import * as CONSTANT from '../constants';
 import {
-
-} from './../actions';
+  updateMovieResult,
+  analyseMovies,
+  updateSingleMovie } from '../actions';
 
 
 describe('FilterForm Actions', () => {
-  describe('updateFilterGenre()', () => {
-    let value;
-    let response;
+  describe('updateMovieResult()', () => {
+    let active;
     let error;
     beforeEach(() => {
-      value = 'value';
-      response = 'response';
+      active = 'value';
       error = 'error';
     });
 
+    it('Should call actions from updateMovieResult - requested, succeed, failed', () => {
+      const expected = {
+        request: {
+          type: CONSTANT.UPDATE_MOVIE_RESULT.REQUEST,
+        },
+        success: {
+          type: CONSTANT.UPDATE_MOVIE_RESULT.SUCCESS,
+          active,
+        },
+        failure: {
+          type: CONSTANT.UPDATE_MOVIE_RESULT.FAILURE,
+          error,
+        },
+      };
+
+      expect(updateMovieResult.request()).to.eql(expected.request);
+      expect(updateMovieResult.success(active)).to.eql(expected.success);
+      expect(updateMovieResult.failure(error)).to.eql(expected.failure);
+    });
+  });
+
+  describe('analyseMovies()', () => {
+    let pending;
+    let notSorted;
+    let error;
+    beforeEach(() => {
+      pending = [];
+      notSorted = [];
+      error = 'error';
+    });
+
+    it('Should call actions from analyseMovies - requested, succeed, failed', () => {
+      const expected = {
+        request: {
+          type: CONSTANT.ANALYSE_MOVIE.REQUEST,
+          notSorted,
+        },
+        success: {
+          type: CONSTANT.ANALYSE_MOVIE.SUCCESS,
+          pending,
+        },
+        failure: {
+          type: CONSTANT.ANALYSE_MOVIE.FAILURE,
+          error,
+        },
+      };
+
+      expect(analyseMovies.request(notSorted)).to.eql(expected.request);
+      expect(analyseMovies.success(pending)).to.eql(expected.success);
+      expect(analyseMovies.failure(error)).to.eql(expected.failure);
+    });
+  });
+
+  describe('updateSingleMovie()', () => {
+    let active;
+    let removePending;
+    let error;
+    beforeEach(() => {
+      active = {};
+      removePending = [];
+      error = 'error';
+    });
+
+    it('Should call actions from updateSingleMovie - requested, succeed, failed', () => {
+      const expected = {
+        request: {
+          type: CONSTANT.UPDATE_SINGLE_MOVIE.REQUEST,
+          active,
+        },
+        success: {
+          type: CONSTANT.UPDATE_SINGLE_MOVIE.SUCCESS,
+          removePending,
+        },
+        failure: {
+          type: CONSTANT.UPDATE_SINGLE_MOVIE.FAILURE,
+          error,
+        },
+      };
+
+      expect(updateSingleMovie.request(active)).to.eql(expected.request);
+      expect(updateSingleMovie.success(removePending)).to.eql(expected.success);
+      expect(updateSingleMovie.failure(error)).to.eql(expected.failure);
+    });
   });
 });
