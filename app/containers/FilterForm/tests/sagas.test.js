f3e6a324f7a7f67d64e030e5d1632fcf70359943
lanskey <emcioxd@gmail.com>
2016-10-28 15:28:58 +0200

    Update comment

--- app/containers/FilterForm/tests/sagas.test.js
+++ app/containers/FilterForm/tests/sagas.test.js
@@ -1,5 +1,8 @@
 /**
- * Tests for HomePage sagas
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
  */
 
 import { expect } from 'chai';
