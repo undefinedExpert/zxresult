ee3aa16f1424748ff047fe17a134f8f31319c00d
lanskey <emcioxd@gmail.com>
2016-10-28 19:36:26 +0200

    Update tests

--- app/containers/RequestMovie/tests/selectors.test.js
+++ app/containers/RequestMovie/tests/selectors.test.js
@@ -1,3 +1,43 @@
 /**
- * Created by Lansky on 22.10.2016.
+ *  Components are imported in specific (scope based) order:
+ *  1. Node_modules
+ *  2. Application
+ *  3. Module
  */
+
+import { expect } from 'chai';
+import { fromJS } from 'immutable';
+
+import { resultDomain, selectResult } from '../selectors';
+
+
+describe('Result', () => {
+  it('resultDomain()', () => {
+    const resultSelector = resultDomain();
+    const resultState = {
+      data: {},
+    };
+    const mockedState = fromJS({
+      result: fromJS(resultState),
+    });
+
+    expect(resultSelector(mockedState)).to.eql(resultState);
+  });
+
+  it('selectResult()', () => {
+    const resultSelector = selectResult();
+    const resultState = {
+      active: {},
+      pending: {},
+      visited: {},
+      notSorted: {},
+      isFetching: {},
+      noMoreResults: {},
+    };
+    const mockedState = fromJS({
+      result: fromJS(resultState),
+    });
+
+    expect(resultSelector(mockedState)).to.eql(resultState);
+  });
+});
