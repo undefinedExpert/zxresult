4964df2e94023774de1d28b71be24fa200f631f6
lanskey <emcioxd@gmail.com>
2016-11-11 14:38:27 +0100

    Refactor

--- app/components/special/MovieGenres/index.js
+++ app/components/special/MovieGenres/index.js
@@ -13,26 +13,42 @@ import GenreIcons from 'components/special/MovieGenreIcons';
 
 import styles from './styles.css';
 
+
+/**
+ * renderSingle
+ * @desc Render icons list
+ */
+const single = (items, index) => (
+  <li key={index} className={styles.item}>
+    <GenreIcons type={camelCase(items[index])} />
+    <h6 className={styles.label}>
+      {capitalize(items[index])}
+    </h6>
+  </li>
+);
+
+
+/**
+ * renderMultiple
+ * @desc Render icons list
+ */
+const multiple = (items) => (
+  times(items.length, (index) => single(items, index))
+);
+
+
 /**
  * MovieGenres
  * @desc Render Single Select Component, based on it's props.
  * @param { Array } items - What icons our movies contains.
  * @param { String } sectionSize - How much space this section will take, default 1/2 but it
  * might be changed directly in the parent component.
- * TODO: Make this working, it has to use real data and replace all icons with that from API.
  */
 function MovieGenres({ sectionSize = '1/2', items = [] }) {
   return (
     <Section title={'Genres'} size={sectionSize}>
       <ul className={styles.genres}>
-        {times(items.length, (index) => (
-          <li key={index} className={styles.item}>
-            <GenreIcons type={camelCase(items[index])} />
-            <h6 className={styles.label}>
-              {capitalize(items[index])}
-            </h6>
-          </li>
-        ))}
+        {multiple(items)}
       </ul>
     </Section>
   );
