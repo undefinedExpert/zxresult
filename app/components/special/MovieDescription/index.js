c77ef27d5dbbd20c19213773410f918968c09f78
lanskey <emcioxd@gmail.com>
2016-11-29 14:55:44 +0100

    Description fix

--- app/components/special/MovieDescription/index.js
+++ app/components/special/MovieDescription/index.js
@@ -5,8 +5,10 @@
  *  3. Module
  */
 
-import React, { PropTypes as ptype } from 'react';
 import { truncate } from 'lodash';
+import React, { PropTypes as ptype, Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 
 import Section from 'components/general/Section';
 
@@ -16,25 +18,55 @@ import styles from './styles.css';
 /**
  * MovieDescription
  * @desc Check if movie description exist, truncate it if yes, or display appropriate errMsh
- * TODO: Ability to show full description (when user clicks on it)
  * TODO: Refactor error handling, it should not depend on returned message, instead use api call status
  */
-function MovieDescription(props) {
-  const {
-    description,
-    limit,
-    sectionSize = '1/1' } = props;
+class MovieDescription extends Component {
+  state = {
+    isRevealed: false,
+  };
 
-  const title = 'Description';
-  const errMsg = 'Description isn\'t available';
-  const errMsgReturnedByApi = 'No overview found.';
+  handleOnClick = () => {
+    this.setState({ isRevealed: !this.state.isRevealed });
+  };
 
-  const cs = styles.description;
-  return (
-    <Section size={sectionSize} title={title} className={cs}>
-      <p>{description !== errMsgReturnedByApi ? truncate(description, { length: limit }) : errMsg}</p>
-    </Section>
-  );
+  renderDescription = () => {
+    const key = this.state.isRevealed ? 'ON' : 'OFF';
+    const preDesc = truncate(this.props.description, { length: this.props.limit, omission: '', separator: /,?\.* +/ });
+    return (
+      <span key={key} >
+        {this.state.isRevealed ? this.props.description.replace(preDesc, '') : '...'}
+      </span>
+    );
+  };
+
+  render() {
+    const {
+      description,
+      sectionSize = '1/1',
+    } = this.props;
+
+    const title = 'Description';
+    const errMsg = 'Description isn\'t available';
+    const errMsgReturnedByApi = 'No overview found.';
+
+    const cs = styles.description;
+    const key = this.state.isRevealed ? 'ON' : 'OFF';
+    return (
+      <Section size={sectionSize} title={title} className={cs}>
+        <p onClick={this.handleOnClick}>
+          {truncate(this.props.description, { length: this.props.limit, omission: '', separator: /,?\.* +/ })}
+          <ReactCSSTransitionGroup
+            className={styles.animation}
+            transitionName="fade"
+            transitionEnterTimeout={2000}
+            transitionLeaveTimeout={1}
+          >
+            {description !== errMsgReturnedByApi ? this.renderDescription() : errMsg}
+          </ReactCSSTransitionGroup>
+        </p>
+      </Section>
+    );
+  }
 }
 
 MovieDescription.propTypes = {
