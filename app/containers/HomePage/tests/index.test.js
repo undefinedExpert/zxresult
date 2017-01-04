8b21bc3d9dfc7e4bbd4c3d64e528f8d4c3eaa550
lanskey <emcioxd@gmail.com>
2017-01-05 00:09:27 +0100

    Uncomment & update HomePage tests

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -1,24 +1,26 @@
-// import { HomePage } from '../index';
-// import MovieSearchForm from 'containers/MovieSearchForm';
-// import WelcomeText from 'components/WelcomeText';
-//
-// import { expect } from 'chai';
-// import { shallow } from 'enzyme';
-// import React from 'react';
-//
-// describe('<HomePage />', () => {
-//   let renderedComponent;
-//   beforeEach(() => {
-//     renderedComponent = shallow(<HomePage />);
-//   });
-//
-//   it('Should render Movie-search-form', () => {
-//     const expected = renderedComponent.contains(<MovieSearchForm />);
-//     expect(expected).to.eql(true);
-//   });
-//
-//   it('Should render WelcomeText', () => {
-//     const expected = renderedComponent.contains(<WelcomeText />);
-//     expect(expected).to.eql(true);
-//   });
-// });
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import FilterForm from 'containers/FilterForm';
+import WelcomeText from 'components/special/WelcomeText';
+
+import HomePage from '../index';
+
+
+describe('<HomePage />', () => {
+  let renderedComponent;
+  beforeEach(() => {
+    renderedComponent = shallow(<HomePage />);
+  });
+
+  it('Should contain FilterForm', () => {
+    const expected = renderedComponent.contains(<FilterForm />);
+    expect(expected).to.eql(true);
+  });
+
+  it('Should render WelcomeText', () => {
+    const expected = renderedComponent.contains(<WelcomeText />);
+    expect(expected).to.eql(true);
+  });
+});
