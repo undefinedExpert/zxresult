5cd7b87e607842a6f4b8f32f7a67b26f9e425d09
lanskey <emcioxd@gmail.com>
2017-01-04 23:51:34 +0100

    Convert into stateless func

--- app/components/special/MovieResultImage/index.js
+++ app/components/special/MovieResultImage/index.js
@@ -6,29 +6,25 @@
  */
 
 import className from 'classnames';
-import React, { PropTypes as ptype, Component } from 'react';
+import React, { PropTypes as ptype } from 'react';
 
 import LazyImage from 'components/general/LazyImage';
 
 import styles from './styles.css';
 
 
-class MovieResultImage extends Component {
-  render() {
-    return (
-      <div className={className(styles.resultImage)} >
-        <div className={styles.imageContainer}>
-          <LazyImage
-            role="presentation"
-            path={this.props.path}
-            isActive={this.props.isActive}
-            className={className(styles.image)}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+const MovieResultImage = (props) => (
+  <div className={className(styles.resultImage)}>
+    <div className={styles.imageContainer}>
+      <LazyImage
+        role="presentation"
+        path={props.path}
+        isActive={props.isActive}
+        className={className(styles.image)}
+      />
+    </div>
+  </div>
+);
 
 MovieResultImage.propTypes = {
   path: ptype.string,
